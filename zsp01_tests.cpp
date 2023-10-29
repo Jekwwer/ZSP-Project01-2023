40d1ba95015b2b75dc2bdcce18a01beca067b1e8
Evgenii Shiliaev <evgenii.shiliaev.work@pm.me>
2023-10-29 20:42:12 +0100

    changed tests' function for testing other tasks

--- zsp01_tests.cpp
+++ zsp01_tests.cpp
@@ -29,7 +29,7 @@ class StdinStreamBuffer : public std::streambuf
     std::string data;
 };
 
-void runTestWithInput(const std::string &input, std::string &output)
+void runTestWithInputForFunction(const std::string &input, std::string &output, void (*functionToTest)())
 {
     // Create a temporary file and write the input to it
     FILE *tmp = tmpfile();
@@ -42,7 +42,7 @@ void runTestWithInput(const std::string &input, std::string &output)
 
     testing::internal::CaptureStdout();
 
-    u1_1();
+    functionToTest(); // Call the provided function
 
     output = testing::internal::GetCapturedStdout();
 
@@ -50,13 +50,14 @@ void runTestWithInput(const std::string &input, std::string &output)
     fclose(tmp);           // Close and delete the temporary file
 }
 
+// Tests for u1_1
 TEST(U1_1Tests, SampleTest)
 {
     std::string input = "5 100"; // 5 items at a price of 100 each.
     std::string expectedOutput = "Účtenka\nCena bez DPH/ks 100 Kč\tCena s DPH/ks 120 Kč\nPočet kusů: 5\tCena bez DPH "
                                  "500 Kč\tCena s DPH (20 %) 600 Kč\n";
     std::string actualOutput;
-    runTestWithInput(input, actualOutput);
+    runTestWithInputForFunction(input, actualOutput, u1_1);
     ASSERT_EQ(expectedOutput, actualOutput);
 }
 TEST(U1_1Tests, TestSingleItem)
@@ -65,7 +66,7 @@ TEST(U1_1Tests, TestSingleItem)
     std::string expectedOutput = "Účtenka\nCena bez DPH/ks 100 Kč\tCena s DPH/ks 120 Kč\nPočet kusů: 1\tCena bez DPH "
                                  "100 Kč\tCena s DPH (20 %) 120 Kč\n";
     std::string actualOutput;
-    runTestWithInput(input, actualOutput);
+    runTestWithInputForFunction(input, actualOutput, u1_1);
     ASSERT_EQ(expectedOutput, actualOutput);
 }
 
@@ -75,7 +76,7 @@ TEST(U1_1Tests, TestZeroItem)
     std::string expectedOutput = "Účtenka\nCena bez DPH/ks 100 Kč\tCena s DPH/ks 120 Kč\nPočet kusů: 0\tCena bez DPH "
                                  "0 Kč\tCena s DPH (20 %) 0 Kč\n";
     std::string actualOutput;
-    runTestWithInput(input, actualOutput);
+    runTestWithInputForFunction(input, actualOutput, u1_1);
     ASSERT_EQ(expectedOutput, actualOutput);
 }
 
@@ -85,7 +86,7 @@ TEST(U1_1Tests, TestItem_10_Price_50)
     std::string expectedOutput = "Účtenka\nCena bez DPH/ks 50 Kč\tCena s DPH/ks 60 Kč\nPočet kusů: 10\tCena bez DPH "
                                  "500 Kč\tCena s DPH (20 %) 600 Kč\n";
     std::string actualOutput;
-    runTestWithInput(input, actualOutput);
+    runTestWithInputForFunction(input, actualOutput, u1_1);
     ASSERT_EQ(expectedOutput, actualOutput);
 }
 
@@ -95,7 +96,7 @@ TEST(U1_1Tests, TestItem_2_Price_150)
     std::string expectedOutput = "Účtenka\nCena bez DPH/ks 150 Kč\tCena s DPH/ks 180 Kč\nPočet kusů: 2\tCena bez DPH "
                                  "300 Kč\tCena s DPH (20 %) 360 Kč\n";
     std::string actualOutput;
-    runTestWithInput(input, actualOutput);
+    runTestWithInputForFunction(input, actualOutput, u1_1);
     ASSERT_EQ(expectedOutput, actualOutput);
 }
 
@@ -105,7 +106,7 @@ TEST(U1_1Tests, TestItem_3_Price_200)
     std::string expectedOutput = "Účtenka\nCena bez DPH/ks 200 Kč\tCena s DPH/ks 240 Kč\nPočet kusů: 3\tCena bez DPH "
                                  "600 Kč\tCena s DPH (20 %) 720 Kč\n";
     std::string actualOutput;
-    runTestWithInput(input, actualOutput);
+    runTestWithInputForFunction(input, actualOutput, u1_1);
     ASSERT_EQ(expectedOutput, actualOutput);
 }
 
@@ -115,7 +116,7 @@ TEST(U1_1Tests, TestItem_7_Price_70)
     std::string expectedOutput = "Účtenka\nCena bez DPH/ks 70 Kč\tCena s DPH/ks 84 Kč\nPočet kusů: 7\tCena bez DPH 490 "
                                  "Kč\tCena s DPH (20 %) 588 Kč\n";
     std::string actualOutput;
-    runTestWithInput(input, actualOutput);
+    runTestWithInputForFunction(input, actualOutput, u1_1);
     ASSERT_EQ(expectedOutput, actualOutput);
 }
 
@@ -125,7 +126,7 @@ TEST(U1_1Tests, TestItem_1_Price_500)
     std::string expectedOutput = "Účtenka\nCena bez DPH/ks 500 Kč\tCena s DPH/ks 600 Kč\nPočet kusů: 1\tCena bez DPH "
                                  "500 Kč\tCena s DPH (20 %) 600 Kč\n";
     std::string actualOutput;
-    runTestWithInput(input, actualOutput);
+    runTestWithInputForFunction(input, actualOutput, u1_1);
     ASSERT_EQ(expectedOutput, actualOutput);
 }
 
@@ -135,7 +136,7 @@ TEST(U1_1Tests, TestItem_4_Price_25)
     std::string expectedOutput = "Účtenka\nCena bez DPH/ks 25 Kč\tCena s DPH/ks 30 Kč\nPočet kusů: 4\tCena bez DPH 100 "
                                  "Kč\tCena s DPH (20 %) 120 Kč\n";
     std::string actualOutput;
-    runTestWithInput(input, actualOutput);
+    runTestWithInputForFunction(input, actualOutput, u1_1);
     ASSERT_EQ(expectedOutput, actualOutput);
 }
 
@@ -145,7 +146,7 @@ TEST(U1_1Tests, TestItem_15_Price_10)
     std::string expectedOutput = "Účtenka\nCena bez DPH/ks 10 Kč\tCena s DPH/ks 12 Kč\nPočet kusů: 15\tCena bez DPH "
                                  "150 Kč\tCena s DPH (20 %) 180 Kč\n";
     std::string actualOutput;
-    runTestWithInput(input, actualOutput);
+    runTestWithInputForFunction(input, actualOutput, u1_1);
     ASSERT_EQ(expectedOutput, actualOutput);
 }
 
@@ -155,7 +156,7 @@ TEST(U1_1Tests, TestItem_20_Price_300)
     std::string expectedOutput = "Účtenka\nCena bez DPH/ks 300 Kč\tCena s DPH/ks 360 Kč\nPočet kusů: 20\tCena bez DPH "
                                  "6000 Kč\tCena s DPH (20 %) 7200 Kč\n";
     std::string actualOutput;
-    runTestWithInput(input, actualOutput);
+    runTestWithInputForFunction(input, actualOutput, u1_1);
     ASSERT_EQ(expectedOutput, actualOutput);
 }
 
@@ -165,7 +166,7 @@ TEST(U1_1Tests, TestItem_1_Price_1)
     std::string expectedOutput =
         "Účtenka\nCena bez DPH/ks 1 Kč\tCena s DPH/ks 1 Kč\nPočet kusů: 1\tCena bez DPH 1 Kč\tCena s DPH (20 %) 1 Kč\n";
     std::string actualOutput;
-    runTestWithInput(input, actualOutput);
+    runTestWithInputForFunction(input, actualOutput, u1_1);
     ASSERT_EQ(expectedOutput, actualOutput);
 }
 
@@ -175,7 +176,7 @@ TEST(U1_1Tests, TestItem_5_Price_5)
     std::string expectedOutput = "Účtenka\nCena bez DPH/ks 5 Kč\tCena s DPH/ks 6 Kč\nPočet kusů: 5\tCena bez DPH 25 "
                                  "Kč\tCena s DPH (20 %) 30 Kč\n";
     std::string actualOutput;
-    runTestWithInput(input, actualOutput);
+    runTestWithInputForFunction(input, actualOutput, u1_1);
     ASSERT_EQ(expectedOutput, actualOutput);
 }
 
@@ -185,7 +186,7 @@ TEST(U1_1Tests, TestItem_50_Price_25)
     std::string expectedOutput = "Účtenka\nCena bez DPH/ks 25 Kč\tCena s DPH/ks 30 Kč\nPočet kusů: 50\tCena bez DPH "
                                  "1250 Kč\tCena s DPH (20 %) 1500 Kč\n";
     std::string actualOutput;
-    runTestWithInput(input, actualOutput);
+    runTestWithInputForFunction(input, actualOutput, u1_1);
     ASSERT_EQ(expectedOutput, actualOutput);
 }
 
@@ -195,7 +196,7 @@ TEST(U1_1Tests, TestItem_3_Price_7)
     std::string expectedOutput = "Účtenka\nCena bez DPH/ks 7 Kč\tCena s DPH/ks 8 Kč\nPočet kusů: 3\tCena bez DPH 21 "
                                  "Kč\tCena s DPH (20 %) 24 Kč\n";
     std::string actualOutput;
-    runTestWithInput(input, actualOutput);
+    runTestWithInputForFunction(input, actualOutput, u1_1);
     ASSERT_EQ(expectedOutput, actualOutput);
 }
 
@@ -205,7 +206,7 @@ TEST(U1_1Tests, TestItem_17_Price_13)
     std::string expectedOutput = "Účtenka\nCena bez DPH/ks 13 Kč\tCena s DPH/ks 16 Kč\nPočet kusů: 17\tCena bez DPH "
                                  "221 Kč\tCena s DPH (20 %) 272 Kč\n";
     std::string actualOutput;
-    runTestWithInput(input, actualOutput);
+    runTestWithInputForFunction(input, actualOutput, u1_1);
     ASSERT_EQ(expectedOutput, actualOutput);
 }
 
@@ -215,7 +216,7 @@ TEST(U1_1Tests, TestItem_29_Price_19)
     std::string expectedOutput = "Účtenka\nCena bez DPH/ks 19 Kč\tCena s DPH/ks 23 Kč\nPočet kusů: 29\tCena bez DPH "
                                  "551 Kč\tCena s DPH (20 %) 667 Kč\n";
     std::string actualOutput;
-    runTestWithInput(input, actualOutput);
+    runTestWithInputForFunction(input, actualOutput, u1_1);
     ASSERT_EQ(expectedOutput, actualOutput);
 }
 
@@ -225,7 +226,7 @@ TEST(U1_1Tests, TestItem_47_Price_31)
     std::string expectedOutput = "Účtenka\nCena bez DPH/ks 31 Kč\tCena s DPH/ks 37 Kč\nPočet kusů: 47\tCena bez DPH "
                                  "1457 Kč\tCena s DPH (20 %) 1739 Kč\n";
     std::string actualOutput;
-    runTestWithInput(input, actualOutput);
+    runTestWithInputForFunction(input, actualOutput, u1_1);
     ASSERT_EQ(expectedOutput, actualOutput);
 }
 
@@ -235,7 +236,7 @@ TEST(U1_1Tests, TestItem_59_Price_41)
     std::string expectedOutput = "Účtenka\nCena bez DPH/ks 41 Kč\tCena s DPH/ks 49 Kč\nPočet kusů: 59\tCena bez DPH "
                                  "2419 Kč\tCena s DPH (20 %) 2891 Kč\n";
     std::string actualOutput;
-    runTestWithInput(input, actualOutput);
+    runTestWithInputForFunction(input, actualOutput, u1_1);
     ASSERT_EQ(expectedOutput, actualOutput);
 }
 
@@ -245,7 +246,7 @@ TEST(U1_1Tests, TestItem_67_Price_23)
     std::string expectedOutput = "Účtenka\nCena bez DPH/ks 23 Kč\tCena s DPH/ks 28 Kč\nPočet kusů: 67\tCena bez DPH "
                                  "1541 Kč\tCena s DPH (20 %) 1876 Kč\n";
     std::string actualOutput;
-    runTestWithInput(input, actualOutput);
+    runTestWithInputForFunction(input, actualOutput, u1_1);
     ASSERT_EQ(expectedOutput, actualOutput);
 }
 
@@ -255,7 +256,7 @@ TEST(U1_1Tests, TestItem_79_Price_37)
     std::string expectedOutput = "Účtenka\nCena bez DPH/ks 37 Kč\tCena s DPH/ks 44 Kč\nPočet kusů: 79\tCena bez DPH "
                                  "2923 Kč\tCena s DPH (20 %) 3476 Kč\n";
     std::string actualOutput;
-    runTestWithInput(input, actualOutput);
+    runTestWithInputForFunction(input, actualOutput, u1_1);
     ASSERT_EQ(expectedOutput, actualOutput);
 }
 
@@ -265,7 +266,7 @@ TEST(U1_1Tests, TestItem_89_Price_53)
     std::string expectedOutput = "Účtenka\nCena bez DPH/ks 53 Kč\tCena s DPH/ks 64 Kč\nPočet kusů: 89\tCena bez DPH "
                                  "4717 Kč\tCena s DPH (20 %) 5696 Kč\n";
     std::string actualOutput;
-    runTestWithInput(input, actualOutput);
+    runTestWithInputForFunction(input, actualOutput, u1_1);
     ASSERT_EQ(expectedOutput, actualOutput);
 }
 
@@ -275,7 +276,7 @@ TEST(U1_1Tests, TestItem_97_Price_29)
     std::string expectedOutput = "Účtenka\nCena bez DPH/ks 29 Kč\tCena s DPH/ks 35 Kč\nPočet kusů: 97\tCena bez DPH "
                                  "2813 Kč\tCena s DPH (20 %) 3395 Kč\n";
     std::string actualOutput;
-    runTestWithInput(input, actualOutput);
+    runTestWithInputForFunction(input, actualOutput, u1_1);
     ASSERT_EQ(expectedOutput, actualOutput);
 }
 
@@ -285,7 +286,7 @@ TEST(U1_1Tests, TestItem_101_Price_43)
     std::string expectedOutput = "Účtenka\nCena bez DPH/ks 43 Kč\tCena s DPH/ks 52 Kč\nPočet kusů: 101\tCena bez DPH "
                                  "4343 Kč\tCena s DPH (20 %) 5252 Kč\n";
     std::string actualOutput;
-    runTestWithInput(input, actualOutput);
+    runTestWithInputForFunction(input, actualOutput, u1_1);
     ASSERT_EQ(expectedOutput, actualOutput);
 }
 
