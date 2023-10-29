fc801aaf9850d8f11eae8c896d20f1179a86777e
Evgenii Shiliaev <evgenii.shiliaev.work@pm.me>
2023-10-29 17:14:04 +0100

    added main.cpp

---
+++ main.cpp
@@ -0,0 +1,33 @@
+/**
+ * @file main.cpp
+ * @brief Application entry point.
+ *
+ * This file contains the main function and serves as the entry point for the application.
+ * A part of my solution to the 1st homework assignment
+ * in the ZSP VŠPJ (Fundamentals of Structural Programming) course.
+ * 
+ * This file is NOT part of the submitted solution.
+ * It's meant only for debugging and testing purposes.
+ *
+ * @see https://github.com/Jekwwer/ZSP-Homework01-2023
+ *
+ * @author Evgenii Shiliaev
+ * @date October 29, 2023
+ */
+
+#include "zsp01_functions.h"
+
+/**
+ * @brief Main function of the application.
+ *
+ * This function initializes the application,
+ * and performs the primary logic of the program.
+ *
+ * @return nothing
+ */
+int main()
+{
+    // todo main
+}
+
+/** End of main.cpp */
