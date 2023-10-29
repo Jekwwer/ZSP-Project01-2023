7fde9fdd02ee01c240e8b125116f9f93a9d555cc
Evgenii Shiliaev <evgenii.shiliaev.work@pm.me>
2023-10-29 15:31:49 +0100

    added header file for zsp01_functions.cpp

---
+++ zsp01_functions.h
@@ -0,0 +1,29 @@
+﻿/**
+ * @file zsp01_functions.h
+ * @brief Declarations and Definitions for ZSP VŠPJ 1st Homework Assignment.
+ *
+ * This header file for the 1st homework assignment
+ * in the ZSP VŠPJ (Fundamentals of Structural Programming) course.
+ *
+ * This file is NOT part of the submitted solution.
+ * It's meant only for debugging and testing purposes.
+ *
+ * @see https://github.com/Jekwwer/ZSP-Homework01-2023
+ *
+ * @author Evgenii Shiliaev
+ * @date October 29, 2023
+ */
+
+#ifndef ZSP_FUNCTIONS01_H
+#define ZSP_FUNCTIONS01_H
+#include <iostream>
+
+void u1_1();
+
+void u1_2();
+
+void u1_3();
+
+#endif // ZSP_FUNCTIONS01_H
+
+/** End of zsp01_functions.h */
